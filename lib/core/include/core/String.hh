af30c9e5972fc3b1ba01e7fcfd2e030390520a24
Eryn Wells <eryn@erynwells.me>
2015-10-17 00:37:17 -0700

    Add String.hh

---
+++ lib/core/include/core/String.hh
@@ -0,0 +1,18 @@
+/* String.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Strings are fun.
+ */
+
+#include <string>
+
+
+namespace erw {
+namespace core {
+
+typedef std::string String;
+
+} /* namespace core */
+} /* namespace erw */
